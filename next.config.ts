6745fd73febd40d2951a27eee535ec53e1229e12
Your Name <vaibhav514garg@gmail.com>
2025-07-30 03:02:47 +0530

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Prevent Webpack from bundling the native `canvas` module
-    config.resolve = config.resolve || {};
-    config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      canvas: false,
-      fs: false,
-      path: false,
-    };
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   // Prevent Webpack from bundling the native `canvas` module
+  //   config.resolve = config.resolve || {};
+  //   config.resolve.fallback = {
+  //     ...(config.resolve.fallback || {}),
+  //     canvas: false,
+  //     fs: false,
+  //     path: false,
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
